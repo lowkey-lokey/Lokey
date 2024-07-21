56fa494b059ae302148b64e2506ddadf6b134496
lowkey-lokey <lokeyanimates@gmail.com>
2024-07-21 12:44:26 +0800

    Update VRButton.js

--- libs/VRButton.js
+++ libs/VRButton.js
@@ -186,7 +186,7 @@ class VRButton{
         if (!ignorePadding) element.style.padding = '12px 6px';
         element.style.border = '1px solid #fff';
         element.style.borderRadius = '4px';
-        element.style.background = (active) ? 'rgba(20,150,80,1)' : 'rgba(180,20,20,1)';
+        element.style.background = (active) ? 'rgba(125,90,177,1)' : 'rgba(180,20,20,1)';
         element.style.color = '#fff';
         element.style.font = `normal ${fontSize}px sans-serif`;
         element.style.textAlign = 'center';
