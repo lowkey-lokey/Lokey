48e06a5c63663111c0f224f917d1345f8f90798e
lowkey-lokey <lokeyanimates@gmail.com>
2024-07-21 13:13:24 +0800

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ class App{
         
 		this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.01, 500 );
         this.camera.position.set(0, 1.6, 0);
-        this.camera.rotation.set(0, 180, 0);
+        this.camera.rotation.set(0, 225, 0);
 
 
 
